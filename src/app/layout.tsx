faa0fba283014c5fb3ba1ebb1144f83cc1828d31
Jannatul Ferdous <ferdousmunmun75@gmail.com>
2026-07-16 04:44:52 +0600

    font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "LittleNest",
@@ -28,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+     className={`${poppins.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Navbar/>
